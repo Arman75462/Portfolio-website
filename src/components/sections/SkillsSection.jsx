40e3d273c673bf842a959a7f9821cbdaeb2ba8ae
Arman <armangulistani@gmail.com>
2024-07-25 16:00:02 -0400

    Completed SkillsSection

--- src/components/sections/SkillsSection.jsx
+++ src/components/sections/SkillsSection.jsx
@@ -7,6 +7,46 @@ function SkillsSection({ language }) {
       <h2 className="SkillsSection__title section__title">
         {data[language].SkillsSection.title}
       </h2>
+
+      <h3 className="SkillsSection__subtitle">
+        {data[language].SkillsSection.skillsCard.frontEndTools.title}
+      </h3>
+      <article className="SkillsSection__skills-container">
+        {data[language].SkillsSection.skillsCard.frontEndTools.skills.map(
+          (skill) => (
+            <div key={skill.id} className="skill-container">
+              <img
+                loading="lazy"
+                src={skill.imageSource}
+                alt={skill.imageAlt}
+                className="skill__icon"
+              />
+              <p className="skill__name">{skill.name}</p>
+              <p className="skill-number">{skill.id}</p>
+            </div>
+          )
+        )}
+      </article>
+
+      <h3 className="SkillsSection__subtitle">
+        {data[language].SkillsSection.skillsCard.devTools.title}
+      </h3>
+      <article className="SkillsSection__skills-container">
+        {data[language].SkillsSection.skillsCard.devTools.skills.map(
+          (skill) => (
+            <div key={skill.id} className="skill-container">
+              <img
+                loading="lazy"
+                src={skill.imageSource}
+                alt={skill.imageAlt}
+                className="skill__icon"
+              />
+              <p className="skill__name">{skill.name}</p>
+              <p className="skill-number">{skill.id}</p>
+            </div>
+          )
+        )}
+      </article>
     </section>
   );
 }
