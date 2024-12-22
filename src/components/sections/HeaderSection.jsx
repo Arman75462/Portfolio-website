59cd95a75af26189ff401251b30c4e84d131a88c
Arman <armangulistani@gmail.com>
2024-12-22 15:26:24 -0500

    Added isScrolled state and HeaderSection__detachable selector
    When the client has scrolled more than 50px from top to bottom, the header will detach itself from its fixed position to move slightly below its initial position. Done for aesthetics purposes.

--- src/components/sections/HeaderSection.jsx
+++ src/components/sections/HeaderSection.jsx
@@ -8,7 +8,9 @@ import NavigationSection from "/src/components/sections/NavigationSection.jsx";
 function HeaderSection({ isDarkMode, setIsDarkMode, language, setLanguage }) {
   const [menuOpen, setIsMenuOpen] = useState(false);
   const [isScreenLarge, setIsScreenLarge] = useState(window.innerWidth > 1000);
+  const [isScrolled, setIsScrolled] = useState(window.scrollY > 50);
 
+  // Store the width of the window for knowing when to add hamburger menu and when not to.
   useEffect(() => {
     function handleResize() {
       setIsScreenLarge(window.innerWidth > 1000);
@@ -20,12 +22,30 @@ function HeaderSection({ isDarkMode, setIsDarkMode, language, setLanguage }) {
     };
   }, []);
 
+  // Store the amount the client scrolled each time scrollY changes in value for knowing when to display detachable header and when not to.
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   function handleHamburgerMenuClick() {
     setIsMenuOpen((prevMenuOpen) => !prevMenuOpen);
   }
 
   return (
-    <header className="HeaderSection">
+    <header
+      className={
+        isScrolled ? "HeaderSection__detachable" : "HeaderSection__sticked"
+      }
+    >
       <p className="HeaderSection__initials">A.G.</p>
 
       {isScreenLarge ? (
