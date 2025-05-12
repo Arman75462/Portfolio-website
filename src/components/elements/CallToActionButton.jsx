1c2757fcf1fab93da42aa0e4f118cf2962ae5f4f
Arman <armangulistani@gmail.com>
2025-05-11 20:54:14 -0400

    Open PDF in new tab instead of downloading
    Replaced the download CV functionality with opening the PDF in a new browser tab. This gives user the the option to either read or download the resume. Also updated the icon and comment to reflect this behavior.

--- src/components/elements/CallToActionButton.jsx
+++ src/components/elements/CallToActionButton.jsx
@@ -1,9 +1,19 @@
 import "/src/styles/elements-styles/CallToActionButton.css";
 
-function CallToActionButton({ className, link, children, ...rest }) {
+function CallToActionButton({
+  className,
+  link = null,
+  children,
+  onClick,
+  ...rest
+}) {
   return (
     <a href={link} {...rest}>
-      <button type="button" className={`CallToActionButton ${className}`}>
+      <button
+        type="button"
+        className={`CallToActionButton ${className}`}
+        onClick={onClick}
+      >
         {children}
       </button>
     </a>
